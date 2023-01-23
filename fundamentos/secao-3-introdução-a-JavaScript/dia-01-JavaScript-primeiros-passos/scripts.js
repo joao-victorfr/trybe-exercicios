5562269dbb732dce235e1e3f7f14ba97cf02d1d8
João Victor <joaovictorfranco9@gmail.com>
2023-01-23 14:41:28 -0300

    Adicionando exercicios dia 01 - variáveis

---
+++ fundamentos/secao-3-introdução-a-JavaScript/dia-01-JavaScript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+const myName = "João Victor";
+const birthCity = "Pará de Minas";
+let birthYear = 1998;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+//birthCity = "Belo Horizonte";//Não é possível alterar devido a variável ser constante.
