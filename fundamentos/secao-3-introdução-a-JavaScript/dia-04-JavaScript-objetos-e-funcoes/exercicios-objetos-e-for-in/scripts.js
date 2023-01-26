ffad3c2a7671d6ca84d4251d5309e941a70b10ba
João Victor <joaovictorfranco9@gmail.com>
2023-01-26 15:37:35 -0300

    Finalizando último requisito

--- fundamentos/secao-3-introdução-a-JavaScript/dia-04-JavaScript-objetos-e-funcoes/exercicios-objetos-e-for-in/scripts.js
+++ fundamentos/secao-3-introdução-a-JavaScript/dia-04-JavaScript-objetos-e-funcoes/exercicios-objetos-e-for-in/scripts.js
@@ -1,12 +1,16 @@
 let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+        personagem: 'Margarida',
+        origem: 'Pato Donald',
+        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+        recorrente : "Sim",
+        personagem2: "Tio Patinhas",
+        origem2: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+        nota2: 'O último MacPatinhas',
+        recorrente2 : "Sim",
 };
 
-console.log("Bem vinda "+ info.personagem);
 
-info.recorrente = "Sim";
+console.log("Bem vinda "+ info.personagem);
 
 console.log(info.recorrente);
 
@@ -16,4 +20,14 @@ for(let key in info){
 
 for(let key in info){
     console.log(info[key]);
+}
+
+
+console.log(info.personagem + " e "+ info.personagem2);
+console.log(info.origem + " e "+ info.origem2);
+console.log(info.nota + " e "+ info.nota2);
+if(info.recorrente === info.recorrente2){
+    console.log("Ambos recorrente");
+}else{
+    console.log(info.recorrente + " e "+ info.recorrente2);
 }
